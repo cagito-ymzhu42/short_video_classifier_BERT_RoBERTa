255ca9cecdd3bc33cdb95add35be3a560b6905c8
cagito-ymzhu42 <57518227+cagito-ymzhu42@users.noreply.github.com>
2024-09-11 00:41:40 +0800

    Add files via upload

---
+++ pretrained_pipeline/model/ner/crf_bert/crf_bert.py
@@ -0,0 +1,135 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from transformers import AutoConfig
+from transformers import AutoModel
+
+import torch
+from torch import nn
+from pretrained_pipeline.nn.base.basemodel import BasicModel
+from pretrained_pipeline.nn.layer.crf_block import CRFLayer
+from pretrained_pipeline.nn.nezha.modeling import NeZhaModel
+from pretrained_pipeline.nn.layer.downs_encoder import BiLSTM, RTransformer, TENER
+from pretrained_pipeline.model.ner.crf_bert.utils import Params
+
+class CrfBert(BasicModel):
+    """
+    crf bert model
+    适配各种下游模型
+    """
+    def __init__(self, config, ds_encoder_type='LSTM'):
+        super(CrfBert, self).__init__()
+        params = Params(pre_model_type=config.pre_model_type, ex_index=0, fold_id=0)
+        self.num_labels = len(params.tags)
+
+        # pretrain model
+        self.pre_model_type = params.pre_model_type
+        if self.pre_model_type == 'NEZHA':
+            self.bert = NeZhaModel.from_pretrained(config.bert_pretrained_name)
+        elif self.pre_model_type == 'RoBERTa':
+            model_config = AutoConfig.from_pretrained(config.bert_pretrained_name)
+            self.bert = AutoModel.from_pretrained(config.bert_pretrained_name, config=model_config)
+        else:
+            raise ValueError('Pre-train Model type must be NEZHA or ELECTRA or RoBERTa!')
+
+        # 动态权重
+        self.fusion_layers = params.fusion_layers
+        self.dym_weight = nn.Parameter(torch.ones((self.fusion_layers, 1, 1, 1)),
+                                       requires_grad=True)
+
+        # downstream encoder
+        self.ds_encoder = ds_encoder_type
+        if self.ds_encoder == 'LSTM':
+            self.bilstm = BiLSTM(self.num_labels, embedding_size=config.hidden_size, hidden_size=params.lstm_hidden,
+                                 num_layers=params.lstm_layer,
+                                 dropout=params.drop_prob, with_ln=True)
+        elif self.ds_encoder == 'TENER':
+            self.tener = TENER(tag_size=self.num_labels, embed_size=config.hidden_size, dropout=params.drop_prob,
+                               num_layers=params.num_layers, d_model=params.tener_hs, n_head=params.num_heads)
+        elif self.ds_encoder == 'RTRANS':
+            self.rtrans = RTransformer(tag_size=self.num_labels, dropout=params.drop_prob, d_model=config.hidden_size,
+                                       ksize=params.k_size, h=params.rtrans_heads)
+        else:
+            raise ValueError('Downstream encoder type must be LSTM or TENER or RTRANS!')
+
+        # crf
+        self.crf = CRFLayer(self.num_labels, params)
+
+        # init weights
+        self.init_weights()
+        self.reset_params()
+
+    def reset_params(self):
+        nn.init.xavier_normal_(self.dym_weight)
+
+    def get_dym_layer(self, outputs):
+        """获取动态权重融合后的BERT output.
+        Args:
+            outputs: origin bert output
+        Returns:
+            sequence_output: 融合后的bert encoder output. (bs, seq_len, hs)
+        """
+        if self.pre_model_type in ('ELECTRA', 'NEZHA'):
+            fusion_idx = 0
+        elif self.pre_model_type == 'RoBERTa':
+            fusion_idx = 2
+        else:
+            raise ValueError('Pre-train Model type must be NEZHA or ELECTRA or RoBERTa!')
+
+        hidden_stack = torch.stack(outputs[fusion_idx][-self.fusion_layers:],
+                                   dim=0)  # (num_layers, bs, seq_len, hs)
+        sequence_output = torch.sum(hidden_stack * self.dym_weight,
+                                    dim=0)  # (bs, seq_len, hs)
+        return sequence_output
+
+    def forward(
+            self,
+            input_ids=None,
+            attention_mask=None,
+            token_type_ids=None,
+            labels=None,
+    ):
+        """
+        Args:
+            input_ids: (batch_size, seq_len)
+            attention_mask: 各元素的值为0或1，避免在padding的token上计算attention。(batch_size, seq_len)
+            token_type_ids: 就是token对应的句子类型id，值为0或1。为空自动生成全0。(batch_size, seq_len)
+            labels: (batch_size, seq_len)
+        """
+        # pre-train model
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            output_hidden_states=True
+        )  # sequence_output, pooled_output, (hidden_states), (attentions)
+        sequence_output = outputs[-1][-1]
+        # fusion BERT layers
+        # sequence_output = self.get_dym_layer(outputs)  # (batch_size, seq_len, hidden_size[embedding_dim])
+        batch_size, seq_len, tag_size = sequence_output.size()
+
+        if self.ds_encoder == 'LSTM':
+            # (seq_len, batch_size, tag_size)
+            feats = self.bilstm.get_lstm_features(sequence_output.transpose(1, 0), attention_mask.transpose(1, 0))
+        elif self.ds_encoder == 'TENER':
+            feats = self.tener(sequence_output, attention_mask)  # (bs, seq_len, tag_size)
+            feats = feats.transpose(1, 0)
+        elif self.ds_encoder == 'RTRANS':
+            feats = self.rtrans(sequence_output, attention_mask)
+            feats = feats.transpose(1, 0)
+        else:
+            raise TypeError('Downstream encoder type must be LSTM or TENER or RTRANS!')
+
+        if labels is not None:
+            # CRF
+            # total scores
+            forward_score = self.crf(feats, attention_mask.transpose(1, 0))
+            gold_score = self.crf.score_sentence(feats, labels.transpose(1, 0),
+                                                 attention_mask.transpose(1, 0))
+            loss = (forward_score - gold_score).sum() / batch_size
+            return loss
+        else:
+            # 维特比算法
+            best_paths = self.crf.viterbi_decode(feats, attention_mask.transpose(1, 0))
+            return best_paths
+
---
+++ pretrained_pipeline/model/text_cls/nezha_cls_model.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+from torch import nn
+import torch
+
+from pretrained_pipeline.nn.base.basemodel import BasicModel
+from pretrained_pipeline.nn.nezha.modeling import NeZhaModel
+from pretrained_pipeline.nn.nezha.configuration import NeZhaConfig
+
+
+class NezhaClsModel(BasicModel):
+    """
+    nezha cls模型
+    """
+    def __init__(self, config):
+        super(NezhaClsModel, self).__init__()
+        self.n_classes = config.n_classes
+        # self.bert = NeZhaModel(config)
+        self.bert = NeZhaModel.from_pretrained(config.bert_pretrained_name)
+        embedding_dim = self.bert.config.to_dict()['hidden_size']
+        self.classifier = nn.Linear(embedding_dim, self.n_classes)
+
+    def forward(self, input_ids=None, attention_mask=None, token_type_ids=None, labels=None):
+        # attention_mask = torch.ne(input_ids, 0)  #todo
+
+        encoder_out, pooled_out = self.bert(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids
+        )
+        logits = self.classifier(pooled_out)
+        # outputs = (logits,) + (pooled_out,)
+
+        # if labels is not None:
+        #     loss_fct = nn.CrossEntropyLoss()
+        #     # loss_fct = focal_loss()
+        #     loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+        #     outputs = (loss,) + outputs
+
+        # return outputs
+        return logits
+
+
+class NezhaAttClsModel(BasicModel):
+    def __init__(self, config):
+        super().__init__()
+        model_config = NeZhaConfig.from_pretrained(config.bert_pretrained_name)
+        model_config.update({"output_hidden_states": True})
+        self.nezha = NeZhaModel.from_pretrained(config.bert_pretrained_name, config=model_config)
+
+        dim = self.nezha.pooler.dense.bias.shape[0]
+
+        self.dropout = nn.Dropout(p=0.2)
+        self.high_dropout = nn.Dropout(p=0.5)
+
+        n_weights = 12
+        weights_init = torch.zeros(n_weights).float()
+        weights_init.data[:-1] = -3
+        self.layer_weights = torch.nn.Parameter(weights_init)
+
+        self.attention = nn.Sequential(
+            nn.Linear(768, 768),
+            nn.Tanh(),
+            nn.Linear(768, 1),
+            nn.Softmax(dim=1)
+        )
+        self.cls = nn.Sequential(
+            nn.Linear(dim, config.n_classes)
+        )
+        self.init_params([self.cls, self.attention])
+
+    def init_params(self, layers):
+        for layer in layers:
+            for m in layer.modules():
+                if isinstance(m, nn.Linear):
+                    nn.init.xavier_uniform_(m.weight.data)
+                    nn.init.constant_(m.bias.data, 0)
+
+
+    def forward(self, input_ids, attention_mask):
+        nezha_output = self.nezha(input_ids=input_ids,
+                                      attention_mask=attention_mask)
+
+        cls_outputs = torch.stack(
+            [self.dropout(layer) for layer in nezha_output[2][-12:]], dim=0
+        )
+        cls_output = (
+                torch.softmax(self.layer_weights, dim=0).unsqueeze(1).unsqueeze(1).unsqueeze(1) * cls_outputs).sum(
+            0)
+
+        logits = torch.mean(
+            torch.stack(
+                [torch.sum(self.attention(self.high_dropout(cls_output)) * cls_output, dim=1) for _ in range(5)],
+                dim=0,
+            ),
+            dim=0,
+        )
+        return self.cls(logits)
